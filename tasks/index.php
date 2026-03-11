58b1ced275fc6ba61194af96ac2c37a524e9e4f1
DavyWDK <77610877+DavyWDK@users.noreply.github.com>
2026-03-11 13:38:19 +0100

    fdsfs
    sdfasf

---
+++ tasks/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>IndexTasks</title>
+        <?php require_once 'create.php'; ?>
+    </head>
+
+    <body>
+        <?php require_once '../head.php'; ?>
+
+        <div class="container">
+            <h1>Nieuwe Taak</h1>
+
+            <form action="../app/Http/Controllers/tasksController.php" method="POST">
+                <div class="form-group">
+                    <label for="titel">Titel:</label>
+                    <input type="text" name="titel" id="titel" class="form-input">
+                </div>
+                <div class="form-group"
+        
+            </form>
+        </div>
+
+    </body>
+</html>
